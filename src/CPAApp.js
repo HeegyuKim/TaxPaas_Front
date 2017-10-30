e63d03d082a0db7fb769984a68dc2bc4c61f7a25
HeegyuKim <heekue83@gmail.com>
2017-10-30 15:38:42 +0900

    add client url router

---
+++ src/CPAApp.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+
+import Header from './components/header/Header'
+import Dashboard from './components/dashboard/Dashboard'
+import TaxOrganizer from './components/taxorg/TaxOrganizer'
+import OCRLayout from './components/taxorg/OCRLayout'
+import ClientProfile from './components/client/ClientProfile'
+
+import './stylesheets/Base.css'
+
+class CPAApp extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          cpa/client/1234/ocr -> 1234 -> hash
+
+          <Route exact path="/cpa/client/:clientId/ocr" component={OCRLayout} />
+          <Route exact path='/cpa/client/:clientId/taxorg' component={TaxOrganizer} />
+          <Route exact path='/cpa/client/:clientId' component={ClientProfile}/>
+          <Route exact path='/cpa' component={Dashboard}/>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default CPAApp;
--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-import CPAApp from './CPAApp'
-import ClientApp from './ClientApp'
+import 'semantic-ui-css/semantic.min.css';
 import './stylesheets/Base.css'
 
-class App extends Component {
+class ClientApp extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/cpa/" component={CPAApp} />
-          <Route path="/client/" component={ClientApp} />
         </div>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default ClientApp;
