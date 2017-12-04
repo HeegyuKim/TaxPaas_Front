77cea42e95b3ba389f28cee0a4ed46d0babaa764
HeegyuKim <heekue83@gmail.com>
2017-12-04 15:22:20 +0900

    add img url

--- src/components/dashboard/SimpleClientList.js
+++ src/components/dashboard/SimpleClientList.js
@@ -7,16 +7,16 @@ export default class SimpleClientList extends React.Component {
     super(props)
     this.state = {
       clients : [
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_01.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_02.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_03.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_04.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_05.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_06.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_07.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_08.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_09.jpg"},
-        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"img/profile_10.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_01.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_02.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_03.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_04.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_05.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_06.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_07.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_08.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_09.jpg"},
+        {name:"Name",ssn:'******1234',type:'Business', profileUrl:"/img/profile_10.jpg"},
       ]
     }
   }
